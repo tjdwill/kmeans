5ea558b44e605d0ef6755a4670c493aa9ce397e3
tjdwill <118497355+tjdwill@users.noreply.github.com>
2024-04-08 23:35:56 -0500

    Add files via upload
    Update names, docstrings, and imports

--- kmeans/__init__.py
+++ kmeans/__init__.py
@@ -1,4 +1,3 @@
 from kmeans.clustering import cluster
-from kmeans.base_funcs import SMALLEST_THRESH
 from kmeans.animate import view_clustering
-__all__ = ["cluster", "view_clustering", "SMALLEST_THRESH"]
+__all__ = ["cluster", "view_clustering"]
